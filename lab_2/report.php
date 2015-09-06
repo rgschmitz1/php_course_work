dd477c4c98123c45002af90a2dc3ed999be52f25
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-09-06 19:04:11 +0000

    finished test drive on page 42

--- lab_2/report.php
+++ lab_2/report.php
@@ -5,16 +5,23 @@
 <body>
   <h2>Aliens Abducted Me - Report an Abduction</h2>
 <?php
+  $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
   $when_it_happened = $_POST['whenithappened'];
   $how_long = $_POST['howlong'];
+  $how_many = $_POST['howmany'];
   $alien_description = $_POST['aliendescription'];
+  $what_they_did = $_POST['whattheydid'];
   $fang_spotted = $_POST['fangspotted'];
+  $other = $_POST['other'];
   $email = $_POST['email'];
 
-  echo 'Thanks for submitting the form.<br/>';
-  echo 'You were abducted ' . $when_it_happened;
-  echo ' and were gone for ' . $how_long . '<br/>';
-  echo 'Describe them: ' . $alien_description . '<br/>';
-  echo 'Was Fang there? ' . $fang_spotted . '<br/>';
-  echo 'Your email address is ' . $email;
+  echo 'Thanks for submitting the form, <b>' . $name . '</b>.<br/>';
+  echo 'You were abducted <b>' . $when_it_happened;
+  echo '</b> and were gone for <b>' . $how_long . '</b><br/>';
+  echo 'Number or aliens: <b>' . $how_many . '</b><br/>';
+  echo 'Describe them: <b>' . $alien_description . '</b><br/>';
+  echo 'The aliens did this: <b>' . $what_they_did . '</b><br/>';
+  echo 'Was Fang there? <b>' . $fang_spotted . '</b><br/>';
+  echo 'Other comments: <b>' . $other . '</b><br/>';
+  echo 'Your email address is <b>' . $email . '</b>';
 ?>
