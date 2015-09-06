ba80d78dfc5a14c2fb1d08e2809c3779d7f7b922
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-09-06 20:10:02 +0000

    finished test drive on page 52

--- lab_2/report.php
+++ lab_2/report.php
@@ -5,6 +5,7 @@
 <body>
   <h2>Aliens Abducted Me - Report an Abduction</h2>
 <?php
+  /* user inputs */
   $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
   $when_it_happened = $_POST['whenithappened'];
   $how_long = $_POST['howlong'];
@@ -15,6 +16,20 @@
   $other = $_POST['other'];
   $email = $_POST['email'];
 
+  /* email variables */
+  $to = 'rgschmitz1@madisoncollege.edu';
+  $subject = 'Aliens Abducted Me - Abduction Report';
+  $msg = "$name was abducted $when_it_happened and was gone for $how_long.\n" .
+    "Number of aliens: $how_many\n" .
+    "Alien description: $alien_description\n" .
+    "What they did: $what_they_did\n" .
+    "Fang spotted: $fang_spotted\n" .
+    "Other comments: $other";
+
+  /* send email */
+  mail($to, $subject, $msg, "From:$email");
+
+  /* confirmation page */
   echo 'Thanks for submitting the form, <b>' . $name . '</b>.<br/>';
   echo 'You were abducted <b>' . $when_it_happened;
   echo '</b> and were gone for <b>' . $how_long . '</b><br/>';
