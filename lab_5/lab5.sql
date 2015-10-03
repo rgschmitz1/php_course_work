0b925f92b5b5e961de5c90adb7dc3b655e165f95
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-10-03 21:19:03 +0000

    finished test drive on page 235

---
+++ lab_5/lab5.sql
@@ -0,0 +1,5 @@
+#ALTER TABLE <table> ADD COLUMN <column name> <data type>
+#ALTER TABLE <table> DROP COLUMN <column name>
+#ALTER TABLE <table> CHANGE COLUMN <column name> <new column name> <data type>
+#ALTER TABLE <table> MODIFY COLUMN <column name> <data type> <position>
+ALTER TABLE guitarwars ADD COLUMN screenshot varchar(64);
