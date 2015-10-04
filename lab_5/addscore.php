c68377a0c9ffbf9e590462a59e173f1dccf4a243
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-10-04 13:36:36 +0000

    finished test drive on page 243

--- lab_5/addscore.php
+++ lab_5/addscore.php
@@ -14,13 +14,14 @@
     // Grab the score data from the POST
     $name = $_POST['name'];
     $score = $_POST['score'];
+    $screenshot = $_FILES['screenshot']['name'];
 
-    if (!empty($name) && !empty($score)) {
+    if (!empty($name) && !empty($score) && !empty($screenshot)) {
       // Connect to the database
       $dbc = mysqli_connect('localhost', 'rgschmitz11', '', 'gwdb');
 
       // Write the data to the database
-      $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score')";
+      $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
       mysqli_query($dbc, $query);
 
       // Confirm success with the user
@@ -32,6 +33,7 @@
       // Clear the score data to clear the form
       $name = "";
       $score = "";
+      $screenshot = "";
 
       mysqli_close($dbc);
     }
@@ -42,13 +44,18 @@
 ?>
 
   <hr />
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="hidden" name="MAX_FILE_SIZE" value="32768" />
     <label for="name">Name:</label>
-    <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>" /><br />
+    <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>" />
+    <br />
     <label for="score">Score:</label>
     <input type="text" id="score" name="score" value="<?php if (!empty($score)) echo $score; ?>" />
+    <br />
+    <label for="screenshot">Screen shot:</label>
+    <input type="file" id="screenshot" name="screenshot" />
     <hr />
     <input type="submit" value="Add" name="submit" />
   </form>
-</body> 
+</body>
 </html>
