9e7db4a597f50015c8a5838214d66abe0e57fa81
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-10-05 22:15:38 +0000

    finished test drive on page 290 (additional cleanup)

--- lab_5/admin.php
+++ lab_5/admin.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -19,19 +17,23 @@
   $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
   // Retrieve the score data from MySQL
-  $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
+  $query = 'SELECT * FROM guitarwars ORDER BY score DESC, date ASC';
   $data = mysqli_query($dbc, $query);
 
   // Loop through the array of score data, formatting it as HTML
   echo '<table>';
   while ($row = mysqli_fetch_array($data)) {
+    $id = $row['id'];
+    $date = $row['date'];
+    $name = $row['name'];
+    $score = $row['score'];
+    $screenshot = $row['screenshot'];
     // Display the score data
-    echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
-    echo '<td>' . $row['date'] . '</td>';
-    echo '<td>' . $row['score'] . '</td>';
-    echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] .
-      '&amp;name=' . $row['name'] . '&amp;score=' . $row['score'] .
-      '&amp;screenshot=' . $row['screenshot'] . '">Remove</a></td></tr>';
+    echo "<tr class='scorerow'><td><strong>$name</strong></td>";
+    echo "<td>$date</td>";
+    echo "<td>$score</td>";
+    echo "<td><a href='removescore.php?id=$id&amp;date=$date&amp;name=$name" .
+      "&amp;score=$score&amp;screenshot=$screenshot'>Remove</a></td></tr>";
   }
   echo '</table>';
 
--- lab_5/removescore.php
+++ lab_5/removescore.php
@@ -37,26 +37,27 @@
       $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
       // Delete the score data from the database
-      $query = "DELETE FROM guitarwars WHERE id = $id LIMIT 1";
+      $query = "DELETE FROM guitarwars WHERE id = '$id' LIMIT 1";
       mysqli_query($dbc, $query);
       mysqli_close($dbc);
 
       // Confirm success with the user
-      echo '<p>The high score of ' . $score . ' for ' . $name . ' was successfully removed.';
+      echo "<p>The high score of $score for $name was successfully removed.";
     } else {
       echo '<p class="error">The high score was not removed.</p>';
     }
   } else if (isset($id) && isset($name) && isset($date) && isset($score)) {
     echo '<p>Are you sure you want to delete the following high score?</p>';
-    echo '<p><strong>Name: </strong>' . $name . '<br /><strong>Date: </strong>' . $date .
-      '<br /><strong>Score: </strong>' . $score . '</p>';
+    echo "<p><strong>Name: </strong>$name<br/>" .
+      "<strong>Date: </strong>$date<br/>" .
+      "<strong>Score: </strong>$score</p>";
     echo '<form method="post" action="removescore.php">';
     echo '<input type="radio" name="confirm" value="Yes" /> Yes ';
     echo '<input type="radio" name="confirm" value="No" checked="checked" /> No <br />';
     echo '<input type="submit" value="Submit" name="submit" />';
-    echo '<input type="hidden" name="id" value="' . $id . '" />';
-    echo '<input type="hidden" name="name" value="' . $name . '" />';
-    echo '<input type="hidden" name="score" value="' . $score . '" />';
+    echo "<input type='hidden' name='id' value='$id' />";
+    echo "<input type='hidden' name='name' value='$name' />";
+    echo "<input type='hidden' name='score' value='$score' />";
     echo '</form>';
   }
 
