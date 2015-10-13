0fb0fa1c2694530d96c72be5bb1346d4b125dbfb
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-10-13 01:01:34 +0000

    finished test drive on page 340

--- lab_6/addscore.php
+++ lab_6/addscore.php
@@ -13,15 +13,18 @@
   require_once('appvars.php');
   require_once('connectvars.php');
 
+  // Connect to the database
+  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
   if (isset($_POST['submit'])) {
     // Grab the score data from the POST
-    $name = $_POST['name'];
-    $score = $_POST['score'];
-    $screenshot = $_FILES['screenshot']['name'];
+    $name = mysqli_real_escape_string($dbc, trim($_POST['name']));
+    $score = mysqli_real_escape_string($dbc, trim($_POST['score']));
+    $screenshot = mysqli_real_escape_string($dbc, trim($_FILES['screenshot']['name']));
     $screenshot_type = $_FILES['screenshot']['type'];
     $screenshot_size = $_FILES['screenshot']['size'];
 
-    if (!empty($name) && !empty($score) && !empty($screenshot)) {
+    if (!empty($name) && is_numeric($score) && !empty($screenshot)) {
       if ((($screenshot_type == 'image/gif') || ($screenshot_type == 'image/jpeg') ||
         ($screenshot_type == 'image/pjpeg') || ($screenshot_type == 'image/png')) &&
         ($screenshot_size > 0) && ($screenshot_size <= GW_MAXFILESIZE)) {
@@ -29,11 +32,10 @@
           // Move the file to the target upload folder
           $target = GW_UPLOADPATH . $screenshot;
           if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {
-            // Connect to the database
-            $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
             // Write the data to the database
-            $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot', 0)";
+            $query = "INSERT INTO guitarwars (date, name, score, screenshot)" .
+              "VALUES (NOW(), '$name', '$score', '$screenshot')";
             mysqli_query($dbc, $query);
 
             // Confirm success with the user
