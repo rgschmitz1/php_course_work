8e14420f03bbcdd6885123fa9b0873753ea1b78b
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-10-26 15:19:50 +0000

    finished test drive on page 531

--- lab_8/search.php
+++ lab_8/search.php
@@ -53,13 +53,15 @@
   }
   $result = mysqli_query($dbc, $query);
   while ($row = mysqli_fetch_array($result)) {
-    echo '<tr class="results">';
-    echo '<td valign="top" width="20%">' . $row['title'] . '</td>';
-    echo '<td valign="top" width="50%">' . $row['description'] . '</td>';
-    echo '<td valign="top" width="10%">' . $row['state'] . '</td>';
-    echo '<td valign="top" width="20%">' . $row['date_posted'] . '</td>';
-    echo '</tr>';
-  } 
+?>
+    <tr class="results">
+      <td valign="top" width="20%"><?= $row['title'] ?></td>
+      <td valign="top" width="50%"><?= substr($row['description'], 0, 100) ?>...</td>
+      <td valign="top" width="10%"><?= $row['state'] ?></td>
+      <td valign="top" width="20%"><?= substr($row['date_posted'], 0, 10) ?></td>
+    </tr>
+<?php
+  }
 
   mysqli_close($dbc);
 ?>
