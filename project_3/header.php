b9211baeb8f48d9e7f828882cd40c097d8f19bdb
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-11-23 09:17:52 +0000

    Adding footer.html to index.php

--- project_3/header.php
+++ project_3/header.php
@@ -16,19 +16,19 @@
 
 <body>
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?= SITE_ROOT ?>/index.php">Project 3</a>
-    </div>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= SITE_ROOT ?>/index.php">Project 3</a>
+        </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
 <?php
     session_start();
     if (isset($_SESSION['username']))
@@ -41,7 +41,7 @@
         echo "<li><a href='" . SITE_ROOT . "/login.php'>Login</a></li>";
     }
 ?>
-      </ul>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
