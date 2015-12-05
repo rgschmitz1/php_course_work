d9d5729d6cea083772aff91f8f5d5ea47d31173a
rgschmitz1 <rgschmitz1@madisoncollege.edu>
2015-12-05 21:30:38 +0000

    Adding scripts to footer instead of header

--- project_3/header.php
+++ project_3/header.php
@@ -9,10 +9,6 @@
     <!-- https://bootswatch.com/simplex -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="css/sticky-footer.css" />
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-1.11.3.min.js"></script>
-    <!-- Include all compiled plugins -->
-    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
